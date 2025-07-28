337830431d07ae3934d56d92453401d5ded1b69e
script-js <134544514+script-js@users.noreply.github.com>
2025-07-27 19:07:48 -0700

    fixes

--- creator.js
+++ creator.js
@@ -4,7 +4,8 @@ var instrumentalID;
 var lyricsID;
 
 async function findVideos(song) {
-    progress.style.display = "block"
+    progress.value = 0;
+    progress.style.display = "inline"
     var instrumentals = await (await fetch("https://www.googleapis.com/youtube/v3/search?q=" + song + " instrumental&type=video&videoEmbeddable=true&key=AIzaSyA-ZFrKnMfSPvRnNzk2g7rMNuAEGnCmn00&part=snippet&maxResults=50")).json();
     progress.value++;
     otherInstrumentals = instrumentals.items;
@@ -16,22 +17,22 @@ async function findVideos(song) {
     var lyrics = lyricVideos.items[0].id.videoId;
     progress.value++;
     progress.style.display = "none";
-    songForm.style.marginTop = "20px";
+    songFormContainer.style.marginTop = "20px";
     showPreviews(instrumental, lyrics)
 }
 
-function showPreviews(musicID,lyricID) {
+function showPreviews(musicID, lyricID) {
     instrumentalID = musicID;
     lyricsID = lyricID;
-    showFrame(musicFrame,musicID)
-    showFrame(lyricFrame,lyricID)
+    showFrame(musicFrame, musicID)
+    showFrame(lyricFrame, lyricID)
     vcLeft.style.display = "inline-block";
     vcRight.style.display = "inline-block";
     savebtn.style.display = "block";
     createbtn.style.display = "inline";
 }
 
-function showFrame(frame,id,time) {
+function showFrame(frame, id, time) {
     if (!time) {
         time = 0;
     }
@@ -49,11 +50,14 @@ async function saveSong() {
     } else {
         var saves = [];
     }
-    var vidInfo = await (await fetch("https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=" + instrumentalID)).json();
-    saves.push({
-        title: prompt("Name",song.value),
-        icon: vidInfo.thumbnail_url,
-        url: getKaraokeURL()
-    });
-    localStorage.setItem("saves",JSON.stringify(saves));
+    var saveName = prompt("Name", song.value)
+    if (saveName) {
+        var vidInfo = await (await fetch("https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=" + instrumentalID)).json();
+        saves.push({
+            title: saveName,
+            icon: vidInfo.thumbnail_url,
+            url: getKaraokeURL()
+        });
+        localStorage.setItem("saves", JSON.stringify(saves));
+    }
 }
